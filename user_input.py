d720261695a081b1186bb377871782b53a5e7275
mukoya-davin <mukoyamakokha@gmail.com>
2024-02-23 19:37:51 +0300

    python assignment 1

---
+++ user_input.py
@@ -0,0 +1,7 @@
+# user_input.py
+name = input("What is your name? ")
+age = input("How old are you? ")
+location = input("Where do you stay? ")
+
+# Print out a personalized message using the user's name, age, and location.
+print(f"Hi {name},your age is{age}years old,you stay in{location}.")
